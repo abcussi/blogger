639b5c268d45682308b7f30f0869b56749cd82a3
angel <cussi.angel.benjamin@gmail.com>
2020-04-27 05:40:03 -0400

    Sorcery authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :authors
   resources :articles
   root to: 'articles#index'
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+get 'login'  => 'author_sessions#new'
+get 'logout' => 'author_sessions#destroy'
+
   resources :articles do
     resources :comments
   end
